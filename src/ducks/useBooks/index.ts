25b1ac50536e306b074cd96db7b75eed03d2c8c8
Sakai Rubattel Toma <Sakai-san@users.noreply.github.com>
2020-08-22 00:29:53 +0200

    Cleanup.

--- src/ducks/useBooks/index.ts
+++ src/ducks/useBooks/index.ts
@@ -1,4 +1,3 @@
 import useBooksReducer from "./reducers";
-//export { default as useBooks } from "./useBooksComponent";
 export { default as useBooksOperations } from "./operations";
 export default useBooksReducer;

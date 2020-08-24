9701bc05154f471ff6945f64ea40bc6c0ba004d5
Sakai Rubattel Toma <Sakai-san@users.noreply.github.com>
2020-08-24 23:13:57 +0200

    With axios

--- src/ducks/books/operations.ts
+++ src/ducks/books/operations.ts
@@ -1,15 +1,14 @@
 import { Dispatch } from "redux";
 import actions from "./actions";
+import axios from "axios";
 import b from "./b.json";
 
 const fetchBooks = (dispatch: Dispatch) => {
   let status = "pending";
   let result: any;
 
-  let suspender = fetch(
-    "https://sakai-san.github.io/sharedatadifferentroutes/books.json"
-  )
-    .then((r) => r.json)
+  let suspender = axios
+    .get("https://sakai-san.github.io/sharedatadifferentroutes/books.json")
     .then((r) => {
       status = "success";
       result = r;

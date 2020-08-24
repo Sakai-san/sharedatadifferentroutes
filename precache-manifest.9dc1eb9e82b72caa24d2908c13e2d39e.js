4ec038016ce0f1ac49e0b1a0e77abcaf1b4bf5b5
Sakai Rubattel Toma <Sakai-san@users.noreply.github.com>
2020-08-24 23:14:23 +0200

    Updates

--- precache-manifest.9dc1eb9e82b72caa24d2908c13e2d39e.js
+++ precache-manifest.9dc1eb9e82b72caa24d2908c13e2d39e.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fad1332c42012ce57762187848550bca",
+    "revision": "3329a1ee9e1379510373234a190701b4",
     "url": "/sharedatadifferentroutes/index.html"
   },
   {
-    "revision": "a7021802521544668b3e",
+    "revision": "3b7f8dd35928954ed78d",
     "url": "/sharedatadifferentroutes/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "2395ab285818b9660436",
-    "url": "/sharedatadifferentroutes/static/js/2.907892b3.chunk.js"
+    "revision": "56ae7a7f0d2e2b7ccec1",
+    "url": "/sharedatadifferentroutes/static/js/2.d51f20f8.chunk.js"
   },
   {
     "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
-    "url": "/sharedatadifferentroutes/static/js/2.907892b3.chunk.js.LICENSE.txt"
+    "url": "/sharedatadifferentroutes/static/js/2.d51f20f8.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a7021802521544668b3e",
-    "url": "/sharedatadifferentroutes/static/js/main.9f293872.chunk.js"
+    "revision": "3b7f8dd35928954ed78d",
+    "url": "/sharedatadifferentroutes/static/js/main.4a7308c3.chunk.js"
   },
   {
     "revision": "969aa596226765856e85",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sharedatadifferentroutes/precache-manifest.74ea32ab74018c4701be6e4c79cb2cfd.js"
+  "/sharedatadifferentroutes/precache-manifest.9dc1eb9e82b72caa24d2908c13e2d39e.js"
 );
 
 self.addEventListener('message', (event) => {
